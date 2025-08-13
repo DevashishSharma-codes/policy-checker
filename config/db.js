7ab1a7d85e9a34a3ca2a99b1666fccfe604b3093
Devashish Sharma <devashish2157@gmail.com>
2025-08-14 01:59:53 +0530

    db error solved

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURL = process.env.DB_URL || 'mongodb://0.0.0.0/drive'; // Replace with your actual DB URL
+const dbURL = process.env.ATLAS_URI;
 function connectDb(){
     mongoose.connect(dbURL)
     .then(()=>{
